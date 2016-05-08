73b91e7a1930e684a974b0cccf64402c3795ad09
Tom Tao <tomtao@TomdeMacBook-Pro.local>
2016-05-08 15:09:49 +0800

    Initial Commit
    Basic  function works

---
+++ Final01.ino
@@ -0,0 +1,51 @@
+#include <Servo.h>
+Servo myservo;
+
+
+int pos = 0;
+
+int buttonPin = 2;
+int buttonVal = 0;
+int pButtonVal = 0;
+
+int counter = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  myservo.attach(9);
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  pButtonVal = buttonVal;
+  buttonVal = digitalRead(buttonPin);
+  //Serial.println(buttonVal);
+  Serial.println(counter);
+
+  if (pButtonVal == 1) {
+    if (buttonVal == 0) {
+      counter = 1;
+    } else {
+      counter = 0;
+    }
+  }
+
+  pButtonVal=buttonVal;
+
+  if (counter == 0) { // counter even number
+    pos = 0;
+    myservo.write(pos);
+  } else if (counter == 1) {  // counter odd number
+    for (pos = 0; pos < 180; pos += 1) {
+      myservo.write(pos);
+      delay(30);
+    }
+    for (pos = 180; pos >= 1; pos -= 1) {
+      myservo.write(pos);
+      delay(30);
+    }
+  }
+
+}
+
